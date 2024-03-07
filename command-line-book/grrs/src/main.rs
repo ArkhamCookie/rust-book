557adcdeaa093027d02627c6e68b6454ca2ddc29
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-03-07 14:07:17 -0600

    Fix comments being only 2 slashes

--- command-line-book/grrs/src/main.rs
+++ command-line-book/grrs/src/main.rs
@@ -1,11 +1,11 @@
 use clap::Parser;
 
-// Search for a pattern in a file and display the lines that contain it.
+/// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
-	// The pattern to look for
+	/// The pattern to look for
 	pattern: String,
-	// The path to the file to read
+	/// The path to the file to read
 	path: std::path::PathBuf,
 }
 
