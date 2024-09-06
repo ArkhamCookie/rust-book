f24a3afcecef9bde4396a79314bb49b8910d5595
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-09-06 14:29:00 -0500

    Feat: include statments & expression section

--- chapter-3/functions/src/main.rs
+++ chapter-3/functions/src/main.rs
@@ -2,10 +2,22 @@ fn another_function(x: i32) {
 	println!("The value of x is: {x}");
 }
 
+fn statements_expressions() {
+	let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
+}
+
 // Functions can be defined either before or after the main function.
 fn main() {
 	another_function(5);
 	print_labeled_measurement(5, 'h')
+
+	println!("--- Statements and Expressions ---");
+	statements_expressions();
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
