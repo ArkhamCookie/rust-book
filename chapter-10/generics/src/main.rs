edb628eda8146bfcc60d951721227477691b53b4
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-09-08 23:44:29 -0500

    Feat: Kinda do chapter 10.1

--- chapter-10/generics/src/main.rs
+++ chapter-10/generics/src/main.rs
@@ -1,3 +1,30 @@
+use std::vec;
+
 fn main() {
     println!("--- Generics ---");
+
+    // TODO: Fix after reading chapter
+    /*
+    fn largest<T>(list: &[T]) -> &T {
+        let mut largest = &list[0];
+
+        for number in list {
+            if number > largest {
+                largest = number;
+            }
+
+            largest
+        }
+    }
+
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+
+    println!("The largest number is {result}!");
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(char_list);
+
+    println!("The lartest char is {result}!");
+    */
 }
