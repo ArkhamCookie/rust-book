24299c2b7476e85528e640dacd6d690434a5241a
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-09-09 20:33:24 -0500

    Fix: Update script to new workspace

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find . -type d -path '**/**/target' -exec rm -v -r {} \;
-find . -type f -path '**/**/Cargo.lock' -exec rm -v {} \;
+find . -type d -name 'target' -exec rm -v -r {} \;
+find . -type f -name 'Cargo.lock' -exec rm -v {} \;
