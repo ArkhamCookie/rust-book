9470662c7fac20a75e89b766b21f28a8d211f910
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-09-10 16:52:15 -0500

    Chore: Run `rustfmt`

--- chapter-12/minigrep/src/lib.rs
+++ chapter-12/minigrep/src/lib.rs
@@ -5,7 +5,7 @@ use std::fs;
 pub struct Config {
     pub query: String,
     pub file_path: String,
-	pub ignore_case: bool,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -17,58 +17,54 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-		let ignore_case = env::var("IGNORE_CASE").is_ok();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
-			query,
-			file_path,
-			ignore_case,
-		})
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-	let mut results = Vec::new();
+    let mut results = Vec::new();
 
-	for line in contents.lines() {
-		if line.contains(query) {
-			results.push(line);
-		}
-	}
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
 
-	results
+    results
 }
 
 // TODO: Allow being triggered with arguments
-pub fn search_case_insensitive<'a>(
-	query: &str,
-	contents: &'a str
-) -> Vec<&'a str> {
-	let query = query.to_lowercase();
-	let mut results = Vec::new();
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
 
-	for line in contents.lines() {
-		if line.to_lowercase().contains(&query) {
-			results.push(line);
-		}
-	}
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
 
-	results
+    results
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should be able to read this file");
+    let contents = fs::read_to_string(config.file_path).expect("Should be able to read this file");
 
-	let results = if config.ignore_case {
-		search_case_insensitive(&config.query, &contents)
-	} else {
-		search(&config.query, &contents)
-	};
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
 
-	for line in results {
-		println!("{line}");
-	}
+    for line in results {
+        println!("{line}");
+    }
 
     Ok(())
 }
