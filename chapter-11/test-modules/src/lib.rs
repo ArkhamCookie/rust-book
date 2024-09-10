c5b0081f74fad562d90cdea4ca91fab91b3ca209
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-09-09 20:30:06 -0500

    Feat: Complete chapter 11

--- chapter-11/test-modules/src/lib.rs
+++ chapter-11/test-modules/src/lib.rs
@@ -1,4 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
+pub fn add_two(a: usize) -> usize {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(left: usize, right: usize) -> usize {
     left + right
 }
 
@@ -7,8 +11,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
+    fn internal() {
+        let result = internal_adder(2, 2);
 
         assert_eq!(result, 4);
     }
