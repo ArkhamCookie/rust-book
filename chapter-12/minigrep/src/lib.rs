0d6e590c5095a9cf491435b76a0dcc2ca8972ef2
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-09-11 03:27:21 -0500

    Chore: Run `rustfmt`

--- chapter-12/minigrep/src/lib.rs
+++ chapter-12/minigrep/src/lib.rs
@@ -8,56 +8,56 @@ use clap::{ArgAction, Parser};
 
 #[derive(Parser)]
 pub struct Cli {
-    pub query: String,
-    pub file_path: path::PathBuf,
+	pub query: String,
+	pub file_path: path::PathBuf,
 
-    #[arg(short, long, action = ArgAction::SetTrue)]
-    pub ignore_case: bool,
-    #[arg(short, long, action = ArgAction::SetTrue)]
-    pub verbose: bool,
+	#[arg(short, long, action = ArgAction::SetTrue)]
+	pub ignore_case: bool,
+	#[arg(short, long, action = ArgAction::SetTrue)]
+	pub verbose: bool,
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
+	let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
+	for line in contents.lines() {
+		if line.contains(query) {
+			results.push(line);
+		}
+	}
 
-    results
+	results
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
+	let query = query.to_lowercase();
+	let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
+	for line in contents.lines() {
+		if line.to_lowercase().contains(&query) {
+			results.push(line);
+		}
+	}
 
-    results
+	results
 }
 
 pub fn run(cli: Cli) -> Result<(), Box<dyn Error>> {
-    if cli.verbose {
-        println!("Querying for '{}' in `{:?}`.", cli.query, cli.file_path);
-    }
+	if cli.verbose {
+		println!("Querying for '{}' in `{:?}`.", cli.query, cli.file_path);
+	}
 
-    let contents = fs::read_to_string(cli.file_path).expect("Should be able to read this file");
+	let contents = fs::read_to_string(cli.file_path).expect("Should be able to read this file");
 
-    let results = if cli.ignore_case {
-        search_case_insensitive(&cli.query, &contents)
-    } else {
-        search(&cli.query, &contents)
-    };
+	let results = if cli.ignore_case {
+		search_case_insensitive(&cli.query, &contents)
+	} else {
+		search(&cli.query, &contents)
+	};
 
-    for line in results {
-        println!("{line}");
-    }
+	for line in results {
+		println!("{line}");
+	}
 
-    Ok(())
+	Ok(())
 }
--- chapter-12/minigrep/src/main.rs
+++ chapter-12/minigrep/src/main.rs
@@ -4,12 +4,11 @@ use minigrep::{run, Cli};
 
 use clap::Parser;
 
-
 fn main() {
-    let args = Cli::parse();
+	let args = Cli::parse();
 
-    if let Err(e) = run(args) {
-        eprint!("Application error: {e}");
-        process::exit(1);
-    }
+	if let Err(e) = run(args) {
+		eprint!("Application error: {e}");
+		process::exit(1);
+	}
 }
