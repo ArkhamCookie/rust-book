1b7d7c31dad03502238461b0c6c20aeb285d12d1
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-09-12 22:12:35 -0500

    Fix: Uncomment code commented for testing

--- chapter-13/closures/src/main.rs
+++ chapter-13/closures/src/main.rs
@@ -43,7 +43,7 @@ impl Inventory {
 fn generate_workout(intensity: u32, random_number: u32) {
 	let expensive_closure = |num: u32| -> u32 {
 		println!("calculating slowly...");
-		// thread::sleep(Duration::from_secs(2));
+		thread::sleep(Duration::from_secs(2));
 		num
 	};
 
